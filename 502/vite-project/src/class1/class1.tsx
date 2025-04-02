39832db90ebd2aa58afd608736c1207885bff09c
marcosdayanm <marcosdayanmann@gmail.com>
2025-04-01 19:22:34 -0600

    Class1

---
+++ 502/vite-project/src/class1/class1.tsx
@@ -0,0 +1,26 @@
+import { greetEs6, greetTraditional } from "./practice_activity_1.ts";
+
+const Class1 = () => {
+  const user = {
+    name: "Marcos",
+    age: 21,
+  };
+
+  return (
+    <>
+      <div>
+        <h3>Activity 1</h3>
+
+        <h4>Using Traditional TS</h4>
+        {greetTraditional(user)}
+        <br />
+        <br />
+
+        <h4>Using ES6+ JavaScript</h4>
+        {greetEs6(user)}
+      </div>
+    </>
+  );
+};
+
+export default Class1;
