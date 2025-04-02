f2dabf960b38ebbb90fec9e87dc1c4c31e6eb54d
marcosdayanm <marcosdayanmann@gmail.com>
2025-04-01 19:31:07 -0600

    deleted react_walkthrough project

--- 502/react_walkthrough/src/class1/practice_activity_1.ts
+++
@@ -1,8 +0,0 @@
-// Before
-export function greetTraditional(user: { name: string; age: number }) {
-  return "Hello, " + user.name + "! You are " + user.age + " years old.";
-}
-
-// After (using template literals + destructuring)
-export const greetEs6 = ({ name, age }: { name: string; age: number }) =>
-  `Hello, ${name}! You are ${age} years old.`;
