76c94771ce6c6f2b3204d66f9bef7ed3059b8914
marcosdayanm <marcosdayanmann@gmail.com>
2025-04-01 21:07:39 -0600

    class2

--- 502/vite-project/src/App.tsx
+++ 502/vite-project/src/App.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
       <div>
@@ -14,11 +17,25 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-
-      <div>
-        <h2>Links Clases Marcos Dayan A01782876</h2>
-        <a href="/src/class1/A01782876/index.html">Ir a la clase 1</a>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
       </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      <a
+        href="/src/class2/A01782876/index.html"
+        id="marcosdayan"
+        className="marcosdayan"
+      >
+        Links Clases Marcos Dayan A01782876
+      </a>
     </>
   );
 }
---
+++ 502/vite-project/src/class2/A01782876/ClassActivity.tsx
@@ -0,0 +1,18 @@
+import LoginForm from "./LoginForm";
+
+import "/src/App.css";
+
+const ClassActivity = () => {
+  return (
+    <>
+      <LoginForm />
+      <br />
+      <br />
+      <a href="/src/index.html">Regresar al menú</a>
+      <br />
+      <a href="/src/class1/A01782876/index.html">Class1</a>
+    </>
+  );
+};
+
+export default ClassActivity;
