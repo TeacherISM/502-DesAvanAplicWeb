3f109a1740fd49de656d1ccde7cd84a1ee1db8c2
Felipe Barbosa <a01784465@tec.mx>
2025-04-02 20:35:40 -0600

    Class2

--- 502/vite-project/src/class1/A01784465/App.tsx
+++ 502/vite-project/src/class1/A01784465/App.tsx
@@ -1,22 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [showIframe, setShowIframe] = useState(false)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+
+      <h1>Login</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -27,7 +19,7 @@ function App() {
       </div>
 
       <div className="card">
-        <a href="./src/class1/A01784465/index.html" target="_blank" rel="noopener noreferrer">
+        <a href="./src/App.tsx" target="_blank" rel="noopener noreferrer">
           <button>Ir a class1/index.html</button>
         </a>
       </div>
