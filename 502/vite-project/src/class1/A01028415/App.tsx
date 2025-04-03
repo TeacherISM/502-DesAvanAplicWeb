29e533156920dbf05f0cecf2c88e1b82ff4b864f
Emiliano Romero <a01028415@tec.mx>
2025-04-02 20:22:00 -0600

    Index

--- 502/vite-project/src/class1/A01028415/App.tsx
+++ 502/vite-project/src/class1/A01028415/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -9,15 +8,8 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+
+      <h1>Emiliano</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,8 +22,8 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p>
-        <a href="src/class1/A01028415/index.html" target="_blank" rel="noopener noreferrer">
-          Emiliano Romero López A01028415
+        <a href="./class1/A01028415/index.html" target="_blank" rel="noopener noreferrer">
+          Ir a index.html en class1
         </a>
       </p>
     </>
