55fb949988ff66468321a8d09eee3c3d6531dd2f
A01749746 <a01749746@tec.mx>
2025-04-02 20:45:11 -0600

    AlbertoLimon-Clase3

--- 502/vite-project/src/App.tsx
+++ 502/vite-project/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './class2/A01749746/Login'
+import TravelRequestForm from './class3/A01749746/TravelRequestForm'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -29,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <TravelRequestForm/>
     </>
   )
 }
