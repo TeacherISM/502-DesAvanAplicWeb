015387bd8eb3048ed50e4ca3750b766635b1b495
Joseph Shakalo <a01784107@tec.mx>
2025-04-09 17:31:09 -0600

    milestone completada

--- 502/vite-project/src/class1/A01784107/menu/Menu.tsx
+++ 502/vite-project/src/class1/A01784107/menu/Menu.tsx
@@ -2,7 +2,7 @@
 
 const ExerciseMenu = () => {
   return (
-    <div style={{ textAlign: 'center', fontFamily: 'Arial, sans-serif' }}>
+    <div style={{ textAlign: 'center', fontFamily: 'Arial, sans-serif',paddingLeft: '450px' }}>
       <h1>Menú de Ejercicios</h1>
       <table style={{ width: '50%', margin: '20px auto', borderCollapse: 'collapse' }}>
         <thead>
@@ -13,23 +13,23 @@ const ExerciseMenu = () => {
         </thead>
         <tbody>
           <tr>
-            <td style={{ border: '1px solid black', padding: '10px' }}>Ejercicio 1</td>
+            <td style={{ border: '1px solid black', padding: '10px' }}>Clase 1</td>
             <td style={{ border: '1px solid black', padding: '10px' }}><a href="/src/class1/A01784107/index.html">Clase 1</a></td>
           </tr>
           <tr>
-            <td style={{ border: '1px solid black', padding: '10px' }}>Ejercicio 2</td>
+            <td style={{ border: '1px solid black', padding: '10px' }}>Clase 2</td>
             <td style={{ border: '1px solid black', padding: '10px' }}><a href="/src/class2/A01784107/index.html">Clase 2</a></td>
           </tr>
           <tr>
-            <td style={{ border: '1px solid black', padding: '10px' }}>Ejercicio 3</td>
+            <td style={{ border: '1px solid black', padding: '10px' }}>Clase 3</td>
             <td style={{ border: '1px solid black', padding: '10px' }}><a href="/src/class3/A01784107/index.html">Clase 3</a></td>
           </tr>
           <tr>
-            <td style={{ border: '1px solid black', padding: '10px' }}>Ejercicio 4</td>
+            <td style={{ border: '1px solid black', padding: '10px' }}>Clase 4</td>
             <td style={{ border: '1px solid black', padding: '10px' }}><a href="/ejercicio4.html">Clase 4</a></td>
           </tr>
           <tr>
-            <td style={{ border: '1px solid black', padding: '10px' }}>Ejercicio 5</td>
+            <td style={{ border: '1px solid black', padding: '10px' }}>Clase 5</td>
             <td style={{ border: '1px solid black', padding: '10px' }}><a href="/ejercicio5.html">Clase 5</a></td>
           </tr>
         </tbody>
