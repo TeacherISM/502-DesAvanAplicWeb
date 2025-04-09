9b793eeff36cba8aac7dc89754af81f43a6909d2
Emily Rosenfeld <a01198339@tec.mx>
2025-04-08 18:48:28 -0600

    Implementacion de Log in

---
+++ 502/Monarca/src/Class2/A01198339/App.tsx
@@ -0,0 +1,12 @@
+import Login from "./Login"
+
+function App() {
+    return (
+      <>
+          <Login />
+          <a href="/src/menu/A01198339/index.html">Menu</a>
+      </>
+  )
+}
+
+export default App
--- 502/Monarca/src/menu/A01198339/App.tsx
+++ 502/Monarca/src/menu/A01198339/App.tsx
@@ -1,15 +1,14 @@
-import "../../App.css"
+import "../../App.css";
 
 function App() {
   return (
-     <>
-     <a href="/src/Class1/A01198339/index.html"> 
-      clase 1
-     </a>
-     <a>
-      clase 2
-     </a>
-     </>
+    <>
+      <nav style={{ display: "flex", flexDirection: "column", gap: "1rem", padding: "1rem" }}>
+        <a href="/src/Class1/A01198339/index.html">Clase 1</a>
+        <a href="/src/Class2/A01198339/index.html">Clase 2</a>
+        <a href="/src/Class3/A01198339/index.html">Clase 3</a>
+      </nav>
+    </>
   );
 }
 
