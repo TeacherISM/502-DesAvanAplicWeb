4332e309b754c54baaf6361f0d8583dc620f8a4d
zuamaya <a01784238@tec.mx>
2025-05-01 16:17:30 -0600

    feat: add base files to class8

---
+++ 502/vite-project/src/class8/A01784238_A01784045/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Class 8</h1>
+      <h2>A01784238 - A01784045</h2>
+      <p>Welcome to the class 8 project!</p>
+    </div>
+  );
+};
+export default App;
