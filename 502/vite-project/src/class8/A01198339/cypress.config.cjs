d398e36b8d2d88d5ec814c6ed62d75a7e4ae2ece
Remy Patgher <remypatgher@Remys-MacBook-Air.local>
2025-05-02 18:28:43 -0600

    js to cjs

--- 502/vite-project/src/class8/A01198339/cypress.config.cjs
+++ 502/vite-project/src/class8/A01198339/cypress.config.cjs
@@ -2,9 +2,9 @@ import  { defineConfig }  from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: "http://localhost:5173",
     supportFile: 'cypress/support/e2e.js',
     fixturesFolder: 'cypress/fixtures',
-    specPattern: 'cypress/e2e//*.cy.{js,tsx}',
+    specPattern: "cypress/e2e/**/*.cy.{ts,tsx}",
   },
 })
